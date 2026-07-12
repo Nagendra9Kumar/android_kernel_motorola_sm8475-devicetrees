9cde8d2c003515db05401b5f8b25f172019e6035
Nagendra <nagendragubbala999+9@gmail.com>
2026-07-12 17:01:46 +0530

    dts: Port tank device trees and configurations from SM7435
    - Import tank specific overlay, dts, and dtsi files.
    - Enable TANK_DTB support in qcom/Makefile, qcom/audio/Kbuild,
      qcom/camera/config/waipio.mk, and qcom/display/Kbuild.
    - Increase qseecom_ta_mem size and apply fastrpc wakeup property fix
      in diwali.dtsi.

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -29,6 +29,10 @@ ifeq ($(CONFIG_LYNKCO_DTB),y)
 dtbo-$(CONFIG_ARCH_DIWALI) += \
 	diwali-camera-sensor-lynkco-evb1.dtbo
 endif  #($(CONFIG_LYNKCO_DTB),y)
+ifeq ($(CONFIG_TANK_DTB),y)
+dtbo-$(CONFIG_ARCH_DIWALI) += \
+	diwali-camera-sensor-tank-evb1.dtbo
+endif  #($(CONFIG_TANK_DTB),y)
 
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
 #remove useless qcom device tree in moto build
